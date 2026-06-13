794cfac28bb2402a4554e8b6ab4d674c08f5665b
gaohao <gaohao.1993@qq.com>
2026-06-13 19:59:35 +0800

    chore(vue.config): 修改注释内容
    将原eslint相关注释改为更通用的语法检查描述，统一表述

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 关闭eslint，eslint的作用是检查代码是否符合vue的规范，这里关闭是为了方便开发
+  // 关闭语法检查，语法检查的作用是检查代码是否符合vue的规范，这里关闭是为了方便开发
   lintOnSave: false
 })
