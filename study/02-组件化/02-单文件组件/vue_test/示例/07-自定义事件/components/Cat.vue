2ee33e30ee2252307b1f00800a2427b6744430e3
gaohao <gaohao.1993@qq.com>
2026-06-15 15:47:45 +0800

    docs: 更新注释并调整示例代码结构
    1. 修正了destroyed钩子的注释描述
    2. 删除了旧的todo相关组件文件和代码
    3. 新增了vue自定义事件的完整示例代码

---
+++ study/02-组件化/02-单文件组件/vue_test/示例/07-自定义事件/components/Cat.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 class="title">{{ name }}</h1>
+    <button @click="getNameFunc">获取名称</button>
+    <button @click="getNameUnbind">取消名称监听</button>
+    <button @click="myDestroy">销毁</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cat',
+  data() {
+    return {
+      name: "小白"
+    }
+  },
+  props: ['getName'],
+  methods: {
+    getNameFunc() {
+      //事件方式
+      this.$emit('getName', this.name)
+      //props方式
+      this.getName(this.name)
+    },
+    getNameUnbind() {
+      // 解绑一个自定义事件
+      this.$off('getName')
+
+      //解绑多个自定义事件
+      // this.$off(['getName', 'getName2'])
+
+      // 解绑所有自定义事件
+      // this.$off()
+    },
+    myDestroy() {
+      // 销毁组件，所有自定义事件也会被解绑
+      // 子组件也会被销毁，子组件的事件也会被解绑
+      // 已经绑定的DOM原生事件还在
+      this.$destroy()
+    }
+  }
+}
+</script>
